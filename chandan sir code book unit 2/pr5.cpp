8c3904c97c49d83ac3428b67ef9f7771bb638201
kunal-chaure <kunalchaure40@gmail.com>
2026-09-18 22:03:12 +0530

    program 6

---
+++ chandan sir code book unit 2/pr5.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+// Base class
+class Vehicle
+{
+protected:
+    string regNo;
+
+public:
+
+    Vehicle(string r)
+    {
+        regNo = r;
+    }
+
+    void start()
+    {
+        cout << "Vehicle " << regNo << " started" << endl;
+    }
+};
+
+
+// Car inherits Vehicle
+class Car : public Vehicle
+{
+public:
+
+    Car(string r) : Vehicle(r)
+    {
+    }
+
+    void openBoot()
+    {
+        cout << "Car boot opened" << endl;
+    }
+};
+
+
+// Bike also inherits Vehicle
+class Bike : public Vehicle
+{
+public:
+
+    Bike(string r) : Vehicle(r)
+    {
+    }
+
+    void helmetReminder()
+    {
+        cout << "Please wear a helmet" << endl;
+    }
+};
+
+
+int main()
+{
+    Car c1("MH12AB1234");
+    Bike b1("MH12CD5678");
+
+
+    c1.start();
+    c1.openBoot();
+
+    b1.start();
+    b1.helmetReminder();
+
+
+    return 0;
+}
---
+++ chandan sir code book unit 2/pr6.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+
+using namespace std;
+
+
+// Academic class
+class Academic
+{
+protected:
+    int marks;
+
+public:
+
+    Academic(int m)
+    {
+        marks = m;
+    }
+
+    void showAcademic()
+    {
+        cout << "Academic Marks: " << marks << endl;
+    }
+};
+
+
+// Sports class
+class Sports
+{
+protected:
+    int sports;
+
+public:
+
+    Sports(int s)
+    {
+        sports = s;
+    }
+
+    void showSports()
+    {
+        cout << "Sports Marks: " << sports << endl;
+    }
+};
+
+
+// Student inherits both classes
+class Student : public Academic, public Sports
+{
+public:
+
+    Student(int a, int s) : Academic(a), Sports(s)
+    {
+    }
+
+    void showTotal()
+    {
+        cout << "Total Marks: " << marks + sports << endl;
+    }
+};
+
+
+int main()
+{
+    Student s1(80, 15);
+
+
+    s1.showAcademic();
+    s1.showSports();
+    s1.showTotal();
+
+
+    return 0;
+}
